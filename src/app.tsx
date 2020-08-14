71dc62b89787fa77fa74e6de42c922f96a0a3d87
Stav Eyal <staveyal210@gmail.com>
2020-08-14 17:41:31 +0300

    Prettified imports

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-// @ts-ignore
-import GridItem from './components/GridItem.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { GridItem } from './components/Grid'
 import './style/grid.scss'
 
 ReactDOM.render(<GridItem thumbnailUrl='img/example.jpg' name='pack' price={120} inStock />,
