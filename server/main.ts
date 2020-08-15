b626e2bf526ce18b29b646b7616233d218eb9c2e
Stav Eyal <stav@tuta.io>
2020-08-16 02:52:20 +0300

    Basic GraphQL  API with mock data

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,66 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
+import { ApolloServer, gql } from 'apollo-server-express'
 
+/**
+ * Create an express instance and apply middleware
+ */
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// Serve static files
 app.use(express.static(path.join(__dirname, '..', 'public')))
 const port: number = process.env.SERVERPORT ? parseInt(process.env.SERVERPORT, 10) : 8080
+
+interface pack {
+  readonly name: string,
+  readonly price: number,
+  readonly thumbnailUrl: string,
+  readonly inStock: boolean
+}
+
+/**
+ * GraphQL type definitions
+ */
+const typeDefs = gql`
+  type Pack {
+    name: String,
+    thumbnailUrl: String,
+    price: Int,
+    inStock: Boolean
+  }
+
+  type Query {
+    packs: [Pack]
+  }
+`
+
+// Mock data
+const packs: pack[] = []
+for (let i = 0; i < 11; i++) {
+  packs.push({
+    name: `חבילה מס ${i}`,
+    thumbnailUrl: '/img/example.jpg',
+    inStock: true,
+    price: Math.floor(Math.random() * 3 + 1) * 120
+  })
+}
+
+const resolvers = {
+  Query: {
+    packs: () => packs
+  }
+}
+
+// Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+// Add Express app
+server.applyMiddleware({ app })
 
 app.listen(port, () => console.log(`Lily Packs listening on port ${port}`))
