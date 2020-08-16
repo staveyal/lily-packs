146399d8984c398e40d081cff66f336b4a0c0c11
Stav Eyal <stav@tuta.io>
2020-08-16 20:22:04 +0300

    Created a GraphQL client connection

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Grid, GridItemProps } from './components/Grid'
+import { Grid } from './components/Grid'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import './style/index.scss'
 
-const packs: GridItemProps[] = []
-for (let i = 0; i < 10; i++) {
-  packs.push({
-    name: `Pack ${i}`,
-    thumbnailUrl: 'img/example.jpg',
-    price: 120,
-    inStock: true
-  })
-}
+// // Mock data
+// const packs: GridItemProps[] = []
+// for (let i = 0; i < 10; i++) {
+//   packs.push({
+//     name: `Pack ${i}`,
+//     thumbnailUrl: 'img/example.jpg',
+//     price: 120,
+//     inStock: true
+//   })
+// }
 
-ReactDOM.render(<Grid packs={packs} />, document.querySelector('#root'))
+/**
+ * Initialising the Apollo client
+ */
+const port: number = typeof process.env.SERVERPORT !== 'undefined' ? parseInt(process.env.SERVERPORT, 10) : 8080
+const client = new ApolloClient({
+  uri: `http://localhost:${port}/graphql`,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(<ApolloProvider client={client}>
+  <Grid />
+</ApolloProvider>, document.querySelector('#root'))
