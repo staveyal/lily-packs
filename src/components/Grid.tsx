7fee4b9aeb28037bcff52da50ecb26c04b02c8e1
Stav Eyal <stav@tuta.io>
2020-08-16 20:23:24 +0300

    Changed Grid component to use GraphQL API for data

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,24 @@
 import React, { FunctionComponent } from 'react'
+import { useQuery, gql, QueryResult } from '@apollo/client'
 
-export interface GridItemProps {
+/**
+ * The GraphQL query for retrieving packs from the server
+ */
+const PACKS = gql`
+  query Packs {
+    packs {
+      name
+      thumbnailUrl
+      price
+      inStock
+    }
+  }
+`
+interface PacksData {
+  packs: Pack[]
+}
+
+export interface Pack {
   readonly name: string // The name of the pack
   readonly thumbnailUrl: string // The URL for the thumbnail picture of the pack
   readonly price: number // Price in shekels
@@ -10,23 +28,29 @@ export interface GridItemProps {
 /**
  * Component code for the items inside the grid of the packs
  */
-const GridItem: FunctionComponent<GridItemProps> =
-({ name, thumbnailUrl, price, inStock }: GridItemProps) =>
+const GridItem: FunctionComponent<Pack> =
+({ name, thumbnailUrl, price, inStock }: Pack) =>
   <a className='grid-item'>
     <img src={thumbnailUrl} alt={name} />
     <p>{name}</p>
   </a>
 
-interface GridProps {
-  readonly packs: GridItemProps[]
-}
+// interface GridProps {
+//   readonly packs: Pack[]
+// }
 
 /**
  * Grid for the packs
  */
-const Grid: FunctionComponent<GridProps> = ({ packs }: GridProps) =>
-  <div className='grid'>
-    { packs.map(pack => <GridItem key={pack.name} {...pack} />)}
-  </div>
+const Grid: FunctionComponent = () => {
+  const { loading, error, data }: QueryResult<PacksData> = useQuery(PACKS)
+  if (loading) return <div>Loading</div>
+  if (error) return <div>Error</div>
+  if (typeof data !== 'undefined') {
+    return <div className='grid'>
+      { data.packs.map(pack => <GridItem key={pack.name} {...pack} />)}
+    </div>
+  } else return <div>Big Error</div>
+}
 
 export { Grid, GridItem }
