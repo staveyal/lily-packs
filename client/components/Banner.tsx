ae393cc976979af29509c3797fd9cdd5757deed1
Stav Eyal <stav@tuta.io>
2020-08-17 20:34:35 +0300

    Started making banner component mockup

---
+++ client/components/Banner.tsx
@@ -0,0 +1,10 @@
+// import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
+
+const Banner = styled.div`
+  width: 100%;
+  height: 4rem;
+  background-color: blue;
+`
+
+export { Banner }
