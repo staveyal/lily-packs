bd09137c6f37d6a00d3aa2d1c892faa7397146f1
Stav Eyal <stav@tuta.io>
2020-08-18 11:33:17 +0300

    Finished banner component

--- client/components/Banner.tsx
+++ client/components/Banner.tsx
@@ -2,9 +2,17 @@
 import styled from 'styled-components'
 
 const Banner = styled.div`
+  background-image: url('/img/banner.jpg');
+  background-color: #000;
   width: 100%;
-  height: 4rem;
-  background-color: blue;
+  height: 20vw;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-bottom: 2rem;
+  @media only screen and (max-width: 768px){
+    height: 40vw;
+  }
 `
 
 export { Banner }
