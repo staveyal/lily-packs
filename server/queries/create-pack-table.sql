2aa68b44a123875254b02fe58066233ca2d0ef6c
Stav Eyal <staveyal210@gmail.com>
2020-08-19 15:19:29 +0300

    Finished table creation and alteration

--- server/queries/create-pack-table.sql
+++ server/queries/create-pack-table.sql
@@ -1,8 +1,23 @@
 CREATE TABLE IF NOT EXISTS packs(
   id serial primary key not null,
   name text not null,
+  img text not null,
   slug text not null,
   price decimal not null,
   specs text not null DEFAULT '',
-  amount int not null
+  show boolean not null DEFAULT true,
+  amount int not null,
+  place int DEFAULT 0
 );
+/*
+Add missing columns if they aren't there
+*/
+ALTER TABLE packs
+  ADD IF NOT EXISTS name text not null,
+  ADD IF NOT EXISTS img text not null,
+  ADD IF NOT EXISTS slug text not null,
+  ADD IF NOT EXISTS price decimal not null,
+  ADD IF NOT EXISTS specs text not null DEFAULT '',
+  ADD IF NOT EXISTS amount int not null,
+  ADD IF NOT EXISTS place int DEFAULT 0,
+  ADD IF NOT EXISTS show boolean not null DEFAULT true
