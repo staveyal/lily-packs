e7129a7aa15b05da6d09e4c2329c5f276159a3a8
Stav Eyal <staveyal210@gmail.com>
2020-08-21 08:31:45 +0300

    Added logo on the banner

--- client/components/Banner.tsx
+++ client/components/Banner.tsx
@@ -1,6 +1,9 @@
+import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
 
-const Banner = styled.div`
+// Image for the banner
+// The aspect ratio is detemined by the height since the width is 100%
+const BannerImage = styled.div`
   background-image: url('/img/banner.jpg');
   background-color: #000;
   width: 100%;
@@ -9,9 +12,79 @@ const Banner = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   margin-bottom: 2rem;
+  @media only screen and (max-width: 1200px) {
+    height: 30vw;
+  }
   @media only screen and (max-width: 768px){
     height: 40vw;
   }
 `
+
+// A gradient with an absolute position to dark the image
+const BannerGradient = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.23), transparent 100%)
+`
+
+// The icon that appears in the middle of the banner
+const LilyIcon = styled.img`
+  /* position: absolute;
+  top: 50%;
+  left: 50%;
+  height: 30vw;
+  width: 30vw;
+  z-index: 3; */
+  position: absolute;
+  top: -1vw;
+  left: calc(50% - 9vw);
+  height: 25vw;
+  width: 25vw;
+  z-index: 3;
+  background-image: url('/img/logo-white.png');
+  @media only screen and (max-width: 1200px) {
+    /* position: absolute;
+    top: 5vh;
+    left: calc(50% - 7.5vw);
+    height: 25vw;
+    width: 25vw;
+    z-index: 3; */
+    position: absolute;
+    top: 1vh;
+    left: calc(50% - 10vw);
+    height: 30vw;
+    width: 30vw;
+    z-index: 3;
+  }
+  @media only screen and (max-width: 768px) {
+    position: absolute;
+    top: 1vh;
+    left: calc(50% - 20vw);
+    height: 40vw;
+    width: 40vw;
+    z-index: 3
+  }
+`
+
+// The underline for the icon
+const Underline = styled.div`
+  position: absolute;
+  top: 2rem;
+  left: 50%;
+  z-index: 2;
+  height: 1.5rem;
+  width: 30vw;
+`
+
+const Banner: FunctionComponent = () => <section>
+  <BannerGradient />
+  <BannerImage>
+    <LilyIcon src='/img/logo-white.png'/>
+  </BannerImage>
+</section>
 
 export { Banner }
