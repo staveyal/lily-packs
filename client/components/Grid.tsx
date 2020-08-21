f5aca8d05b0c91e0c8eed6ace4b03d7b9ba8f8d7
Stav Eyal <staveyal210@gmail.com>
2020-08-21 08:32:45 +0300

    Changed grid to use data from the db, and changed dimensions

--- client/components/Grid.tsx
+++ client/components/Grid.tsx
@@ -7,48 +7,73 @@ import styled from 'styled-components'
  */
 const PACKS = gql`
   query Packs {
-    packs {
-      name
-      thumbnailUrl
-    }
+  Packs {
+    id
+    desc
+    name
+    thumbnailUrl
+    price
+    show
+    slug
   }
+}
 `
 interface PacksData {
-  packs: Pack[]
+  Packs: Pack[]
 }
 
 export interface Pack {
+  readonly id: number
+  readonly desc: string
   readonly name: string // The name of the pack
   readonly thumbnailUrl: string // The URL for the thumbnail picture of the pack
-  readonly inStock?: boolean // Is the pack in stock
+  readonly price: number
+  readonly show: boolean // Is the pack in stock
+  readonly slug: string
 }
 
 const GridLink = styled.a`
   flex-grow: 0;
   flex-shrink: 1;
-  flex-basis: calc(33.33333% - 1.6rem);
+  flex-basis: calc(33.33333% - 2rem);
   display: flex;
   flex-direction: column;
-  font-size: medium;
+  font-size: larger;
   box-sizing: border-box;
   text-align: center;
-  margin: 0 0.8rem;
+  margin: 0 1rem;
   margin-bottom: 2.2rem;
+  @media only screen and (max-width: 1200px) {
+    flex-basis: calc(33% - 1.2rem);
+    margin: 0 0.6rem;
+    margin-bottom: 2.2rem;
+    font-size: larger;
+  }
   @media only screen and (max-width: 768px) {
     flex-basis: calc(50% - 1rem);
     margin: 0 0.5rem;
+    margin-bottom: 2.2rem;
+    font-size: medium;
   }
 `
 
 const Title = styled.p`
-  font-weight: bold;
-  font-size: 1rem;
+  font-weight: 600;
   margin-top: 1rem;
 `
-const Thumbnail = styled.img`
-  height: auto;
+const Thumbnail = styled.div<{ src: string }>`
+  background-image: url(${props => props.src || '/img/banner.jpg'});
+  background-color: #000;
   width: 100%;
+  height: 15vw;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media only screen and (max-width: 768px) {
+    height: 40vw;
+  }
 `
+
 /**
  * Component code for the items inside the grid of the packs
  */
@@ -59,7 +84,7 @@ const GridItem: FunctionComponent<Pack> =
     <Title>{name}</Title>
   </GridLink>
 
-const FlexBox = styled.div`
+const FlexBox = styled.section`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -76,11 +101,11 @@ const FlexBox = styled.div`
  */
 const Grid: FunctionComponent = () => {
   const { loading, error, data }: QueryResult<PacksData> = useQuery(PACKS)
-  if (loading) return <div>Loading</div>
+  if (loading) return <div>בטעינה...</div>
   if (error) return <div>Error</div>
   if (typeof data !== 'undefined') {
     return <FlexBox>
-      { data.packs.map(pack => <GridItem key={pack.name} {...pack} />)}
+      { data.Packs.map(pack => <GridItem key={pack.name} {...pack} />)}
     </FlexBox>
   } else return <div>Big Error</div>
 }
