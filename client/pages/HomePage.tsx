a8d7d92ac224d41d3a09002085ee3ecf67a4f8c5
Stav Eyal <staveyal210@gmail.com>
2020-08-22 01:44:41 +0300

    Added footer element to the home page

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react'
 import { Grid } from '../components/Grid'
 import { Banner } from '../components/Banner'
+import { Footer } from '../components/Footer'
 
 const HomePage: FunctionComponent = () => <div>
   <Banner />
   <Grid />
+  <Footer />
 </div>
 
 export { HomePage }
