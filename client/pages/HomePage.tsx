683a1f2cfa58883b5312df460bd5edb044b5d16d
Stav Eyal <staveyal210@gmail.com>
2020-08-22 16:39:03 +0300

    Added routing capabilities to the home page

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -1,12 +1,30 @@
 import React, { FunctionComponent } from 'react'
+import {
+  Switch,
+  Route,
+  useRouteMatch
+} from 'react-router-dom'
+// import { PackDetails } from './PackDetails'
 import { Grid } from '../components/Grid'
 import { Banner } from '../components/Banner'
 import { Footer } from '../components/Footer'
 
-const HomePage: FunctionComponent = () => <div>
+const GridPage: FunctionComponent = () => <div>
   <Banner />
   <Grid />
   <Footer />
 </div>
+
+const HomePage: FunctionComponent = () => {
+  const match = useRouteMatch()
+  return <Switch>
+    {/* <Route path={`${match.path}/:pack`}>
+      <PackDetails />
+    </Route> */}
+    <Route path={match.path}>
+      <GridPage />
+    </Route>
+  </Switch>
+}
 
 export { HomePage }
