0c9f6814b817392a6a969ff668b13165903c0e11
Stav Eyal <staveyal210@gmail.com>
2020-08-22 23:22:57 +0300

    Implemented pack details page

--- client/pages/PackDetails.tsx
+++ client/pages/PackDetails.tsx
@@ -1,16 +1,27 @@
 import React, { FunctionComponent } from 'react'
+import { Banner, Footer } from 'components'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
 import { gql, useQuery } from '@apollo/client'
 
 const Name = styled.h1`
+  margin-bottom: 1rem;
 `
 const Details = styled.p`
   font-size: medium;
 `
+const Price = styled.span`
+  padding: 0.3rem;
+  font-size: medium;
+  font-weight: 600;
+  color: white;
+  background-color: #5def40;
+  border-radius: 15%;
+  margin: 0.5rem 0;
+`
 const PACK_DETAILS = gql`
-  query {
-    pack {
+  query GetPackDetails($slug: String!) {
+    pack(slug: $slug) {
       id
       name
       price
@@ -20,3 +31,43 @@ const PACK_DETAILS = gql`
     }
   }
 `
+const Image = styled.img`
+  float: left;
+  width: 30%;
+  margin-left: 2rem;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    float: none;
+    margin: auto;
+  }
+`
+
+const DetailSection = styled.section`
+  padding: 0.3rem;
+`
+
+const PackDetails: FunctionComponent = () => {
+  // Get pack slug from the URL
+  const { slug } = useParams()
+  // Get details about the pack from the database using the slug
+  const { loading, error, data } = useQuery(PACK_DETAILS, { variables: { slug } })
+  if (loading) return <p>בטעינה...</p>
+  if (error) return <p>אירעה שגיאה</p>
+  if (typeof data !== 'undefined') {
+    const { name, price, img, specs } = data.pack
+    return <div>
+      <Banner />
+      <DetailSection>
+        <Name>{name}</Name>
+        <Details>{specs.replace('\n', '<br/>')}</Details>
+        <Price>₪{price}</Price>
+        <Image src={img} />
+      </DetailSection>
+      <Footer />
+    </div>
+  } else {
+    return <p>אירעה שגיאה...</p>
+  }
+}
+
+export { PackDetails }
