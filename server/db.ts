10b92608dda33ff09452bf18bbe6304ec3093650
Stav Eyal <staveyal210@gmail.com>
2020-08-22 16:39:25 +0300

    Added query for extra details for each pack

--- server/db.ts
+++ server/db.ts
@@ -6,9 +6,10 @@ const queryPath = path.join(process.cwd(), 'server', 'queries')
 
 // Import queries from files
 const TABLE_PACKS = fs.readFileSync(path.join(queryPath, 'create-pack-table.sql'), 'utf-8')
-// const ADD_PACK = fs.readFileSync(paproductionth.join(queryPath, 'add-pack.sql'), 'utf-8')
 
 const port: number = typeof process.env.PGPORT !== 'undefined' ? parseInt(process.env.PGPORT, 10) : 5432
+
+// Initiate DB connection pool (see node-postgres)
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_USER,
@@ -17,7 +18,7 @@ const pool = new Pool({
   password: process.env.POSTGRES_PASSWORD
 })
 
-// Retry connecting 5 times to PostgreSQL server because of Docker container setup time
+// Retry connecting 5 times to PostgreSQL server because of Docker Compose setup time
 const connect = async ():Promise<void> => {
   for (let i = 5; i !== 0; i--) {
     try {
@@ -38,9 +39,23 @@ const createTable = async ():Promise<void> => {
     .then(() => console.log('Created DB table'))
     .catch(err => console.error(err))
 }
+
+// Connect to the db, then create the schema
 const createConnection = async ():Promise<void> => connect().then(() => createTable())
 
+// Selects all packs that should be visible in the main page
+// Parameters are whether they are in stock, and show=true
 const getVisiblePacks = async ():Promise<any[]> =>
-  pool.query('SELECT * FROM packs WHERE show = true AND amount > 0').then(res => res.rows)
+  pool.query('SELECT id, name, img, price, slug FROM packs WHERE show = true AND amount > 0')
+    .then(res => res.rows)
 
-export { createConnection, getVisiblePacks }
+// Select further details for a specific pack
+const getPackDetails = async (slug: string):Promise<any[]> =>
+  pool.query('SELECT * FROM packs WHERE slug=$1', [slug])
+    .then(res => res.rows[0])
+
+export {
+  createConnection,
+  getVisiblePacks,
+  getPackDetails
+}
